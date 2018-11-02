146d42d8c52ff95cfa41ddfa9acd82b097caef0c
Rohan Kapadia <rohankapadia@gmail.com>
2018-11-02 14:26:05 +0530

    fix local installation.

--- setup/scripts/themes/pop.sh
+++ setup/scripts/themes/pop.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# . ../lib.sh
-
-# as_root "$0" "$@"
-
-# add-apt-repository -y ppa:system76/pop
-# apt-get update
-# apt-get install -y pop-theme
-
 wget -O $HOME/tmp/pop.zip "https://github.com/pop-os/gtk-theme/archive/3.1.2.zip"
 unzip $HOME/tmp/pop.zip -d $HOME/tmp/
 rm $HOME/tmp/pop.zip
 cd $HOME/tmp/gtk-theme-3.1.2
-BASE_DIR=$HOME/.local/share/themes make install
+make gtk2 gtk3 gnome-shell
+make install BASE_DIR=$HOME/.local/share/themes
+rm -rf $HOME/tmp/gtk-theme-3.1.2
