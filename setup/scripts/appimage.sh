34837e4e649a44a721e15353feb6ea58abc3ef42
Rohan Kapadia <rohankapadia@gmail.com>
2018-11-02 13:34:16 +0530

    fix appimage install script.

--- setup/scripts/appimage.sh
+++ setup/scripts/appimage.sh
@@ -4,7 +4,8 @@ wget -O $HOME/tmp/appimaged-x86_64.AppImage \
     "https://github.com/AppImage/appimaged/releases/download/continuous/appimaged-x86_64.AppImage"
 chmod a+x $HOME/tmp/appimaged-x86_64.AppImage
 $HOME/tmp/appimaged-x86_64.AppImage --install
-rm $HOME/tmp/appimaged-x86_64.AppImage
+# rm $HOME/tmp/appimaged-x86_64.AppImage
+# the appimage install command seems to be removing this file on it's own. 
 
 wget -O $HOME/tmp/AppImageUpdate-x86_64.AppImage \
     $(curl -s "https://api.github.com/repos/AppImage/AppImageUpdate/releases/tags/continuous" \
