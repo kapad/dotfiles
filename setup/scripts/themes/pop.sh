7c7c48a18ffb0ad2d89435b6ee999c63d9cf7f16
Rohan Kapadia <rohankapadia@gmail.com>
2018-11-24 19:06:55 +0530

    remove pop theme.
    Because only the cursor was being used, and this is now incompatible for some reason.
    My guess: Either a version conflict, or the make file doesn't include the cursor files.

--- setup/scripts/themes/pop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -O $HOME/tmp/pop.zip "https://github.com/pop-os/gtk-theme/archive/3.1.2.zip"
-unzip $HOME/tmp/pop.zip -d $HOME/tmp/
-rm $HOME/tmp/pop.zip
-cd $HOME/tmp/gtk-theme-3.1.2
-make gtk2 gtk3 gnome-shell
-make install BASE_DIR=$HOME/.local/share/themes
-rm -rf $HOME/tmp/gtk-theme-3.1.2
