6d6db350e5219cfb7ea9f6518d19798500fb87cd
Rohan Kapadia <rohankapadia@gmail.com>
2019-05-12 16:06:37 +0530

    comment out git scripts.
    I haven't really used this project. lacks enough documentation, though some scripts should definitely be used but after I've documented them in a separate repo.

--- setup/git-scripts.sh
+++ setup/git-scripts.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd $HOME/opt
-wget -O git-scripts.zip "https://github.com/jwiegley/git-scripts/archive/master.zip"
-unzip git-scripts.zip
-mv git-scripts-master git-scripts
-rm git-scripts.zip
+# TODO: create my own git-scripts repo. Obviously, starting with ripping off from the below one, but retaining only 
+# what I want to actively keep using. 
+# cd $HOME/opt
+# wget -O git-scripts.zip "https://github.com/jwiegley/git-scripts/archive/master.zip"
+# unzip git-scripts.zip
+# mv git-scripts-master git-scripts
+# rm git-scripts.zip
