ee4f7ffd3b7d7badc6f472374aa679c6be140c84
Rohan Kapadia <rohankapadia@gmail.com>
2020-11-06 16:27:26 +0530

    fix: coloring

--- shell/helpers/powerlevel10k.zsh
+++ shell/helpers/powerlevel10k.zsh
@@ -211,7 +211,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=31
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=39
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
